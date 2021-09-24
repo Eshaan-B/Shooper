e74d0347ac1aad46c792cc9974a8962f7ec4e100
Vedant <singhvedant1701@gmail.com>
2021-09-24 19:49:17 +0530

    Added Start Sreen
    - Added image for app(Not Final Though)
    -Added start Screen

--- lib/screens/userScreen.dart
+++ lib/screens/userScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class UserScreen extends StatefulWidget {
-  static const routeName='/userScreen';
+  static const routeName = '/userScreen';
   @override
   _UserScreenState createState() => _UserScreenState();
 }
@@ -11,7 +11,22 @@ class _UserScreenState extends State<UserScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Test"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/local-shop.png'),
+            ),
+            Text(
+              'Shooper App',
+              style: TextStyle(
+                fontSize: 28,
+                fontFamily: 'sans-serif',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
